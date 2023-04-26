695b65510081546200722156b5409173ff491b27
jorgitorr <jorgearceruiz97@gmail.com>
2023-04-26 21:13:25 +0200

    n

--- ejercicios-PL_SQL(05)-procedimientos-y-funciones.sql
+++ ejercicios-PL_SQL(05)-procedimientos-y-funciones.sql
@@ -71,8 +71,50 @@ END;
 
 /*4) Escribir una función que reciba una fecha y devuelva el año, en número, correspondiente a esa fecha.*/
 
+CREATE OR REPLACE PROCEDURE devuelve_anio_fecha(fecha DATE) IS
+    anio NUMBER := 0;
+BEGIN
+    anio := EXTRACT(YEAR FROM fecha);/*funcion extract(YEAR,MONTH Y DAY PARA EXTRAER UNA PARTE DE LA FECHA)*/
+    DBMS_OUTPUT.PUT_LINE(anio);
+END;
+
+/*OTRA FORMA CON TO_CHAR*/
+
+CREATE OR REPLACE PROCEDURE devuelve_anio_fecha(fecha DATE) IS
+    anio NUMBER := 0;
+BEGIN
+    anio := TO_NUMBER(SUBSTR(TO_CHAR(fecha, 'YYYY-MM-DD'), 1, 4));
+    /*TO_CHAR() -> convierte a cadena de caracteres la fecha, entonces de esa cadena se extrae una parte
+    y el TO_NUMBER convierte a numero
+    TO_NUMBER(), TO_DATE()...*/
+    DBMS_OUTPUT.PUT_LINE(anio);
+END;
+
+DECLARE
+BEGIN
+    devuelve_anio_fecha(DATE '2023-04-26');
+END;
+
+
+/*HECHO CON FUNCTION COMO PIDE: */
+
+CREATE OR REPLACE FUNCTION retorna_anio(fecha DATE) 
+RETURN NUMBER IS
+    anio NUMBER;
+BEGIN
+    anio := EXTRACT(YEAR FROM fecha);
+	RETURN anio;
+END;
+
+
 
 /*5) Escribir un bloque PL/SQL que haga uso de la función anterior.*/
+DECLARE
+    anio NUMBER;
+BEGIN
+  anio := retorna_anio(DATE '2023-04-26');
+	DBMS_OUTPUT.PUT_LINE(anio);
+END;
 
 
 /*6) Dado el siguiente procedimiento:*/
@@ -81,41 +123,131 @@ END;
 CREATE OR REPLACE PROCEDURE crear_depart (
   v_num_dept depart.dept_no%TYPE, 
   v_dnombre depart.dnombre%TYPE DEFAULT 'PROVISIONAL',
-  v_loc     depart.loc%TYPE DEFAULT PROVISIONAL)
+  v_loc  depart.loc%TYPE DEFAULT 'PROVISIONAL')
 IS
 BEGIN
   INSERT INTO depart
-    VALUES (v_num_dept, v_dnombre, v_loc);
+  VALUES (v_num_dept, v_dnombre, v_loc);
 END crear_depart;
 
 
-Indicar cuáles de las siguientes llamadas son correctas y cuáles incorrectas, en este último caso escribir la llamada correcta usando la notación posicional (en los casos que se pueda):
-crear_depart;						-- 1º
-crear_depart(50);					-- 2º	
-crear_depart('COMPRAS');				-- 3º
-crear_depart(50,'COMPRAS');			-- 4º
-crear_depart('COMPRAS', 50);			-- 5º
-crear_depart('COMPRAS', 'VALENCIA');		-- 6º
-crear_depart(50, 'COMPRAS', 'VALENCIA');	-- 7º
-crear_depart('COMPRAS', 50, 'VALENCIA');	-- 8º
-crear_depart('VALENCIA', COMPRAS);		-- 9º
-crear_depart('VALENCIA', 50);  			-- 10º
+/*Indicar cuáles de las siguientes llamadas son correctas y cuáles incorrectas, 
+en este último caso escribir la llamada correcta usando la notación posicional 
+(en los casos que se pueda):*/
+
+crear_depart;						-- 1º /*NO FUNCIONA YA QUE NO SE LE PASA NINGUN PARÁMETRO*/
+crear_depart(50);					-- 2º	/*FUNCIONA YA QUE SE LE PASA EL PARÁMETRO QUE NO TIENE NADA POR DEFECTO*/
+crear_depart('COMPRAS');				-- 3º /*TENDRÍA QUE PASARSELE UN NÚMERO SOLAMENTE O ADEMÁS DE LA CADENA PASARLE UN NÚMERO*/
+crear_depart(50,'COMPRAS');			-- 4º /*FUNCIONA*/
+crear_depart('COMPRAS', 50);			-- 5º /*NO FUNCIONA POR EL ORDEN -> PRIMERO EL NÚMERO*/
+crear_depart('COMPRAS', 'VALENCIA');		-- 6º /*NO FUNCIONA PQ FALTA UN NÚMERO*/
+crear_depart(50, 'COMPRAS', 'VALENCIA');	-- 7º /*FUNCIONA*/
+crear_depart('COMPRAS', 50, 'VALENCIA');	-- 8º /*NO FUNCIONA POR EL ORDEN -> PRIMERO EL NÚMERO*/
+crear_depart('VALENCIA', COMPRAS);		-- 9º /* COMPRAS ENTRE COMILLAS SIMPLES Y EL NÚMERO FALTA*/
+crear_depart('VALENCIA', 50);  			-- 10º /*NO FUNCIONA POR EL ORDEN -> EL NÚMERO SIEMPRE PRIMERO*/
 
 
 /*7) Desarrollar una función que devuelva el número de años completos que hay entre dos fechas que se pasan como argumentos.*/
 
+CREATE OR REPLACE FUNCTION anios_entre_fechas(fecha1 DATE, fecha2 DATE) 
+RETURN NUMBER IS
+BEGIN
+    RETURN EXTRACT(YEAR FROM fecha1) - EXTRACT(YEAR FROM fecha2);
+END;
+
+
+/*USO FUNCIÓN: */
+DECLARE
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(anios_entre_fechas(DATE'1998-12-3',DATE'1865-4-5'));
+END;
+
+/*OTRA FORMA DE DECLARAR VARIABLES DATE LLAMANDO A LA FUNCION*/
+DECLARE
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(anios_entre_fechas(TO_DATE('03/02/1998', 'DD/MM/YYYY'),TO_DATE('03/02/1960', 'DD/MM/YYYY')));
+END;
 
 /*8) Escribir una función que, haciendo uso de la función anterior devuelva los trienios que hay entre dos fechas. (Un trienio son tres años completos).*/
+
+CREATE OR REPLACE FUNCTION trienios_anio(fecha1 DATE, fecha2 DATE)
+RETURN NUMBER
+IS
+trienios NUMBER;
+BEGIN
+  trienios := anios_entre_fechas(fecha1,fecha2);
+	RETURN trienios/3; /*function*/
+END;
+
+/*USO*/
+
+DECLARE 
+    trienios NUMBER;
+BEGIN
+   	trienios:=trienios_anio(DATE'1998-12-3',DATE'1950-12-3'); /*en las fechas el año va al principio*/
+	DBMS_OUTPUT.PUT_LINE(trienios);
+END;
+
+/*con char convirtiendolo a date:*/
+
+DECLARE 
+    trienios NUMBER;
+BEGIN
+   	trienios := trienios_anio(TO_DATE('03/10/2015', 'DD/MM/YYYY'), TO_DATE('1997-10-09', 'YYYY-MM-DD'));
+  DBMS_OUTPUT.PUT_LINE(trienios);
+END;
+
 
 
 /*9) Codificar un procedimiento que reciba una lista de hasta 5 números y visualice su suma.*/
 
 
+CREATE OR REPLACE PROCEDURE suma(num1 NUMBER, num2 NUMBER, num3 NUMBER, num4 NUMBER, num5 NUMBER)
+IS
+    suma NUMBER;
+BEGIN
+    suma := num1 + num2 + num3 + num4 + num5;
+	DBMS_OUTPUT.PUT_LINE(suma);
+END;
+
+/*USO*/
+DECLARE
+BEGIN
+    suma(2,3,4,5,6);
+END;
+
 /*10) Escribir una función que devuelva solamente caracteres alfabéticos sustituyendo cualquier otro carácter por blancos a partir de una cadena que se pasará en la llamada.*/
+
+CREATE OR REPLACE FUNCTION sustituye(cadena VARCHAR2)
+RETURN VARCHAR2 IS
+	nueva_cadena VARCHAR2(50); /*tienes PONERLE UN TAMAÑO A ESTA VARIABLE*/
+BEGIN
+    FOR I IN 1..LENGTH(cadena) LOOP
+    	IF ASCII(SUBSTR(cadena,i,1))>96 AND ASCII(SUBSTR(cadena,i,1))<123 OR ASCII(SUBSTR(cadena,i,1))>64 AND ASCII(SUBSTR(cadena,i,1))<91 THEN
+    		nueva_cadena := nueva_cadena || SUBSTR(cadena,i,1);
+		ELSE 
+            nueva_cadena := nueva_cadena || ' ';
+    	END IF;
+    END LOOP;
+	RETURN nueva_cadena;
+END;
+
+/*USO:*/
+
+DECLARE 
+    cadena VARCHAR2(50);/*TIENES QUE PONERLE UN TAMAÑO A ESTA VARIABLE*/
+BEGIN
+    cadena := sustituye('P5a7co');
+	DBMS_OUTPUT.PUT_LINE(cadena);
+END;
 
 
 /*11) Implementar un procedimiento que reciba un importe y visualice el desglose del cambio en unidades monetarias de 1, 5, 10, 25, 50, 100, 200, 500, 1000, 2000, 5000 Ptas. en orden inverso al que aparecen aquí enumeradas.*/
 
+CREATE OR REPLACE PROCEDURE importe_desglose(num1 NUMBER, num2 NUMBER, num3 NUMBER, num4 NUMBER, num5 NUMBER)IS
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(num5 || num4 || num3 || num2 || num1 );
+END;
 
 /*12) Codificar un procedimiento que permita borrar un empleado cuyo número se pasará en la llamada.
 Nota: El procedimiento anterior devolverá el mensaje 
@@ -139,7 +271,8 @@ Nota: Se necesitará consultar de la tabla del sistema 'USER_OBJECTS' las column
 
 
 
-
+/*PREGUNTARLE A MIGUEL ANGEL PORQUE EN EL EJERCICIO 10 HAY QUE 
+PONERLE UN TAMAÑO A LA VARIABLE*/
 
 
 
