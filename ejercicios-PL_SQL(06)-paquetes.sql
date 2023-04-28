dfcb0613cec2d34eb22cd30f02c82872695581a2
Jorge Arce <65187763+jorgitorr@users.noreply.github.com>
2023-04-28 11:37:38 +0200

    Add files via upload

--- ejercicios-PL_SQL(06)-paquetes.sql
+++ ejercicios-PL_SQL(06)-paquetes.sql
@@ -1,24 +1,72 @@
 --Ejercicios sobre paquetes
 
 
-1) Escribir un paquete completo para gestionar los departamentos. El paquete se llamará gest_depart y deberá incluir, al menos, los siguientes subprogramas:
+/*1) Escribir un paquete completo para gestionar los departamentos. El paquete se llamará gest_depart y deberá incluir, al menos, 
+los siguientes subprogramas:
 
--insertar_nuevo_depart: permite insertar un departamento nuevo. El procedimiento recibe el nombre y la localidad del nuevo departamento. Creará el nuevo departamento comprobando que el nombre no se duplique y le asignará como número de departamento la decena siguiente al último número de departamento utilizado.
+-insertar_nuevo_depart: permite insertar un departamento nuevo. El procedimiento 
+recibe el nombre y la localidad del nuevo departamento. 
+Creará el nuevo departamento comprobando que el nombre no se duplique y le asignará 
+como número de departamento la decena siguiente al último número de departamento utilizado.
 
--borrar_depart: permite borrar un departamento. El procedimiento recibirá dos números de departamento de los cuales el primero corresponde al departamento que queremos borrar y el segundo al departamento al que pasarán los empleados del departamento que se va eliminar. El procedimiento se encargará de realizar los cambios oportunos en los números de departamento de los empleados correspondientes. 
+-borrar_depart: permite borrar un departamento. El procedimiento recibirá dos números de departamento de los cuales el primero corresponde 
+al departamento que queremos borrar y el segundo al departamento al que pasarán los empleados del departamento que se va eliminar. 
+El procedimiento se encargará de realizar los cambios oportunos en los números de departamento de los empleados correspondientes. 
 
--modificar_loc_depart: modifica la localidad del departamento. El procedimiento recibirá el número del departamento a modificar y la nueva localidad, y realizará el cambio solicitado.
+-modificar_loc_depart: modifica la localidad del departamento. El procedimiento recibirá el número del departamento a modificar y la 
+nueva localidad, y realizará el cambio solicitado.
 
--visualizar_datos_depart: visualizará los datos de un departamento cuyo número se pasará en la llamada. Además de los datos relativos al departamento, se visualizará el número de empleados que pertenecen actualmente al departamento. 
+-visualizar_datos_depart: visualizará los datos de un departamento cuyo número se pasará en la llamada. Además de los datos relativos 
+al departamento, se visualizará el número de empleados que pertenecen actualmente al departamento. 
 
--visualizar_datos_depart: versión sobrecargada del procedimiento anterior que, en lugar del número del departamento, recibirá el nombre del departamento. Realizará una llamada a la función buscar_depart_por_nombre que se indica en el apartado siguiente.
+-visualizar_datos_depart: versión sobrecargada del procedimiento anterior que, en lugar del número del departamento, recibirá el nombre 
+del departamento. Realizará una llamada a la función buscar_depart_por_nombre que se indica en el apartado siguiente.
 
 -buscar_depart_por_nombre: función local al paquete. Recibe el nombre de un departamento y devuelve el número del mismo.
+*/
+
+/*a*/
+
+CREATE OR REPLACE PROCEDURE insertar_nuevo_depart(nombre DEPART.DNOMBRE%TYPE,loc DEPART.LOC%TYPE) 
+IS
+    last_depart DEPART.DEPT_NO%TYPE;
+BEGIN
+    SELECT MAX(DEPT_NO) INTO last_depart FROM DEPART;
+    IF last_depart IS NULL THEN
+        last_depart := 10; -- Si no hay departamentos en la tabla, se empieza en 10
+    ELSE
+        last_depart := last_depart + 10; -- Se agrega 10 para asignar el número del nuevo departamento
+    END IF;
+
+    DECLARE
+        num_deps NUMBER;
+    BEGIN
+        SELECT COUNT(*) INTO num_deps FROM DEPART WHERE DNOMBRE = nombre;
+        IF num_deps = 0 THEN
+            INSERT INTO DEPART (DEPT_NO, DNOMBRE, LOC) VALUES (last_depart, nombre, loc);
+        END IF;
+    END;
+END;
+
+/*b NO ESTA TERMINADO*/
+CREATE OR REPLACE PROCEDURE borrar_depart(borrado DEPART.DEPT_NO%type, depart_pasar DEPART.DEPT_NO%type) 
+IS
+BEGIN
+    DECLARE
+    	CURSOR emple_depart IS
+        	SELECT * FROM EMPLE e WHERE e.DEPT_NO=borrado; 
+		depart emple_depart%ROWTYPE;
+    BEGIN
+        FOR depart IN emple_depart LOOP --recorro el cursor
+        	INSERT INTO EMPLE VALUES(depart.EMP_NO,depart.APELLIDO,depart.OFICIO, depart.DIR, depart.FECHA_ALT, depart.SALARIO,
+                depart.COMISION, depart_pasar);
+    	END LOOP;
+	END;
+END;
 
 
 
-
-2) Escribir un paquete completo para gestionar los empleados. El paquete se llamará gest_emple e incluirá, al menos los siguientes subprogramas:
+/*2) Escribir un paquete completo para gestionar los empleados. El paquete se llamará gest_emple e incluirá, al menos los siguientes subprogramas:
 
 -insertar_nuevo_emple
 -borrar_emple. Cuando se borra un empleado todos los empleados que dependían de él pasarán a depender del director del empleado borrado.
@@ -36,5 +84,5 @@ Todos los procedimientos recibirán el número del empleado seguido de los demá
 -subida_salario_pct: incrementará el salario de todos los empleados el porcentaje indicado en la llamada que no podrá ser superior al 25%.
 -subida_salario_imp: sumará al salario de todos los empleados el importe indicado en la llamada. Antes de proceder a la incrementar los salarios se comprobará que el importe indicado no supera el 25% del salario medio. 
 
-
+*/
 
--- ejercicios-PL_SQL(08)-triggers.sql
+++ ejercicios-PL_SQL(08)-triggers.sql
@@ -1,27 +1,109 @@
 --EJEMPLOS DE DISPARADORES Ó TRIGGERS
 
 
-1.- Construir un disparador de base de datos que permita auditar las operaciones de inserción o borrado de datos que se realicen en la tabla emple según las siguientes especificaciones:
+/*1.- Construir un disparador de base de datos que permita auditar las operaciones de inserción o borrado de datos que 
+se realicen en la tabla emple según las siguientes especificaciones:
 -	En primer lugar se creará desde SQL*Plus la tabla auditaremple con la columna col1 VARCHAR2(200).
 -	Cuando se produzca cualquier manipulación se insertará una fila en dicha tabla que contendrá: 
 -	Fecha y hora
 -	Número de empleado
 -	Apellido
--	La operación de actualización INSERCIÓN o BORRADO
+-	La operación de actualización INSERCIÓN o BORRADO*/
 
+CREATE TABLE AUDITAREMPLE(col VARCHAR2(200));
 
-2.- Escribir un trigger de base de datos un que permita auditar las modificaciones en la tabla empleados insertado en la tabla auditaremple los siguientes datos: 
+CREATE OR REPLACE TRIGGER inserta 
+AFTER INSERT OR DELETE 
+ON EMPLE 
+FOR EACH ROW
+BEGIN
+    IF INSERTING THEN 
+    	INSERT INTO AUDITAREMPLE VALUES(TO_CHAR(:NEW.FECHA_ALT,'DD-MON-YYYY')||TO_CHAR(:NEW.EMP_NO,'0000')||' '||:NEW.APELLIDO);
+	ELSIF DELETING THEN
+    	INSERT INTO AUDITAREMPLE VALUES(TO_CHAR(:OLD.FECHA_ALT,'DD-MON-YYYY')||TO_CHAR(:OLD.EMP_NO,'0000')||' '||:OLD.APELLIDO); 
+	END IF;
+END;
+
+/*USO*/
+INSERT INTO EMPLE VALUES(6588, 'FERNANDEZ', 'OFICIAL', 7655, DATE'1998-03-05',
+    5600, 600, 10);
+
+/*2.- Escribir un trigger de base de datos un que permita auditar las modificaciones en la tabla empleados insertado en la tabla auditaremple los siguientes datos: 
 -	Fecha y hora
 -	Número de empleado
 -	Apellido
 -	La operación de actualización: MODIFICACIÓN.
--	El valor anterior y el valor nuevo de cada columna modificada. (solo las columnas modificadas)
+-	El valor anterior y el valor nuevo de cada columna modificada. (solo las columnas modificadas)*/
+
+CREATE OR REPLACE TRIGGER auditar_mod 
+BEFORE UPDATE 
+ON EMPLE 
+FOR EACH ROW
+BEGIN
+    UPDATE AUDITAREMPLE
+    SET col = TO_CHAR(:NEW.FECHA_ALT,'DD-MON-YYYY')||' '||TO_CHAR(:NEW.EMP_NO)||' '||:NEW.APELLIDO;
+END;
+
+/*USO*/
+UPDATE EMPLE e
+SET e.EMP_NO = 20,
+e.FECHA_ALT = DATE'1978-11-23',
+e.APELLIDO = 'Juan'
+WHERE e.APELLIDO = 'JJ';
 
 
-3.- Escribir un disparador de base de datos que haga fallar cualquier operación de modificación del apellido o del número de un empleado, o que suponga una subida de sueldo superior al 10%.
+/*3.- Escribir un disparador de base de datos que haga fallar cualquier operación de modificación del apellido o del número de un empleado, o que suponga una subida de sueldo superior al 10%.*/
 
 
-4.- Suponiendo que disponemos de la vista
+CREATE OR REPLACE TRIGGER falla_mod 
+BEFORE UPDATE 
+ON EMPLE
+FOR EACH ROW 
+BEGIN
+    IF(:NEW.APELLIDO!=:OLD.APELLIDO)THEN
+    	RAISE_APPLICATION_ERROR(-20001,'No se puede modificar un apellido');
+	ELSIF(:NEW.EMP_NO!=:OLD.EMP_NO)THEN
+        RAISE_APPLICATION_ERROR(-20001,'No se puede modificar el numero de empleado');
+    ELSIF(:NEW.SALARIO>:OLD.SALARIO*1.1)THEN 
+    	RAISE_APPLICATION_ERROR(-20001,'No se puede añadir un salario mayor del 10% del anterior');
+    END IF;
+END;
+
+/*uso*/
+
+UPDATE EMPLE
+SET APELLIDO = 'Gutierrez'
+WHERE APELLIDO = 'GUTS';
+
+UPDATE EMPLE
+SET SALARIO = SALARIO*1.2
+WHERE APELLIDO = 'GUTS';
+
+
+UPDATE EMPLE
+SET SALARIO = SALARIO*1.1
+WHERE APELLIDO = 'GUTS';
+
+
+
+/*
+esta mal porque solo se activa cuando hay act de las col indicadas en el OF
+CREATE OR REPLACE TRIGGER falla_mod 
+BEFORE UPDATE 
+OF APELLIDO, EMP_NO ESTE SOLO SE ACTIVA CUANDO HAY ACTUALIZACION DE ESTAS COLUMNAS 
+ON EMPLE
+FOR EACH ROW 
+BEGIN
+    IF(:NEW.SALARIO>:OLD.SALARIO*1.1)THEN
+    	RAISE_APPLICATION_ERROR(-20001,'No se puede añadir un salario mayor del 10% del anterior');
+    END IF;
+END;*/
+
+/*USO:*/
+
+
+
+/*4.- Suponiendo que disponemos de la vista
 
 CREATE VIEW DEPARTAM AS
 SELECT DEPART.DEPT_NO, DNOMBRE, LOC, COUNT(EMP_NO) TOT_EMPLE  
@@ -32,5 +114,9 @@ GROUP BY DEPART.DEPT_NO, DNOMBRE, LOC;
 Construir un disparador que, cuando se pretenda realizar una operación de actualización sobre la vista 'DEPARM', realice la actualización efectiva sobre la tabla 'depart'. En concreto se consideraran las siguientes operaciones de actualización:
 	-Insertar departamento.
 	-Borrar departamento.
-	-Modificar la localidad de un departamento.
+	-Modificar la localidad de un departamento.*/
 
+
+
+/*EL 2 NO SÉ SI ESTÁ BIEN PORQUE NO 
+ENTIENDO MUY BIEN EL ENUNCIADO*/
