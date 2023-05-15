af25a7a0cda26abcb05ee9c5ef4b1e5cf827f95f
jorgitorr <jorgearceruiz97@gmail.com>
2023-05-15 10:32:23 +0200

    n

---
+++ ciclista.sql
@@ -0,0 +1,119 @@
+CREATE TABLE equipo(
+    nomeq VARCHAR2(50),
+    descripcion VARCHAR2(50),
+    CONSTRAINT eq_nom_pk PRIMARY KEY(nomeq)
+);
+
+CREATE TABLE ciclista(
+    dorsal NUMBER,
+    nombre VARCHAR2(50),
+    edad NUMBER,
+    nomeq VARCHAR2(50),
+    CONSTRAINT ci_do_pk PRIMARY KEY(dorsal),
+    CONSTRAINT ci_nom_fk FOREIGN KEY(nomeq) REFERENCES equipo(nomeq)
+);
+
+CREATE TABLE etapa(
+    netapa NUMBER,
+    km NUMBER,
+    salida VARCHAR2(20),
+    llegada VARCHAR2(20),
+    dorsal NUMBER,
+    CONSTRAINT et_dor_fk FOREIGN KEY(dorsal) REFERENCES ciclista(dorsal),
+    CONSTRAINT et_ne_pk PRIMARY KEY(netapa)
+);
+
+CREATE TABLE puerto(
+    nompuerto VARCHAR2(50),
+    altura NUMBER,
+    categoria VARCHAR2(1),
+    pendiente NUMBER,
+    netapa NUMBER,
+    dorsal NUMBER,
+    CONSTRAINT pu_nom_pk PRIMARY KEY(nompuerto),
+    CONSTRAINT pu_ne_fk FOREIGN KEY(netapa) REFERENCES etapa(netapa),
+    CONSTRAINT pu_do_fk FOREIGN KEY(dorsal) REFERENCES ciclista(dorsal)
+);
+
+CREATE TABLE maillot(
+    codigo VARCHAR2(5),
+    tipo VARCHAR2(20),
+    color VARCHAR2(50),
+    premio NUMBER,
+    CONSTRAINT ma_cod_pk PRIMARY KEY(codigo)
+);
+
+CREATE TABLE llevar(
+    dorsal NUMBER,
+    netapa NUMBER,
+    codigo VARCHAR(5),
+    CONSTRAINT ll_ne_fk FOREIGN KEY(netapa) REFERENCES etapa(netapa),
+    CONSTRAINT ll_dor_fk FOREIGN KEY(dorsal) REFERENCES ciclista(dorsal),
+    CONSTRAINT ll_cod_fk FOREIGN KEY(codigo) REFERENCES maillot(codigo)
+);
+
+
+insert into equipo values('Amore Vita','Ricardo Padacci');
+insert into equipo values('Banesto','Miguel EchevarrÃ­a');
+insert into equipo values('Bresciali-Refin','Pietro Armani');
+insert into equipo values('Carrera','Luigi Petroni');
+insert into equipo values('Gatorade','Gian Luca Pacceli');
+insert into equipo values('Kelme','Ãlvaro Pino');
+insert into equipo values('Mapei-Clas','Juan FernÃ¡ndez');
+insert into equipo values('Navigare','Lorenzo Sciacci');
+insert into equipo values('Telecom','Morgan Reikacrd');
+insert into equipo values('TVM','Steevens Henk');
+
+insert into ciclista values(1,'Miguel Indurain',21,'Banesto');
+insert into ciclista values(2,'Pedro Delgado',29,'Banesto');
+insert into ciclista values(3,'Alex Zulle',20,'Navigare');
+insert into ciclista values(4,'Alessio Di Basco',30,'TVM');
+insert into ciclista values(5,'Armand',17,'Amore Vita');
+insert into ciclista values(8,'Jean Van Poppel',24,'Bresciali-Refin');
+insert into ciclista values(9,'Maximo Podel',17,'Telecom');
+insert into ciclista values(10,'Mario Cipollini',31,'Carrera');
+insert into ciclista values(11,'Eddy Seigneur',20,'Amore Vita');
+insert into ciclista values(12,'Alessio Di Basco',34,'Bresciali-Refin');
+insert into ciclista values(13,'Gianni Bugno',24,'Gatorade');
+insert into ciclista values(15,'JesÃºs Montoya',25,'Amore Vita');
+insert into ciclista values(16,'Dimitri Konishev',27,'Amore Vita');
+insert into ciclista values(17,'Bruno Lealli',30,'Amore Vita');
+insert into ciclista values(20,'Alfonso GutiÃ©rrez',27,'Navigare');
+insert into ciclista values(22,'Giorgio Furlan',22,'Kelme');
+insert into ciclista values(26,'Mikel Zarrabeitia',30,'Carrera');
+insert into ciclista values(27,'Laurent Jalabert',22,'Banesto');
+insert into ciclista values(30,'Melchor Mauri',26,'Mapei-Clas');
+insert into ciclista values(31,'Per Pedersen',33,'Banesto');
+insert into ciclista values(32,'Tony Rominger',31,'Kelme');
+insert into ciclista values(33,'Stefenao della Sveitia',26,'Amore Vita');
+insert into ciclista values(34,'Clauido Chiapucci',23,'Amore Vita');
+insert into ciclista values(35,'Gian Mateo Faluca',34,'TVM');
+
+insert into etapa values(1,35,'Valladolid','Ãvila',1);
+insert into etapa values(2,70,'Salamanca','Zamora',2);
+insert into etapa values(3,150,'Zamora','Almendralejo',1);
+insert into etapa values(4,330,'CÃ³rdoba','Granada',1);
+insert into etapa values(5,150,'Granada','AlmerÃ­a',3);
+
+insert into puerto values('p1',2489,'1',34,2,3);
+insert into puerto values('p2',2789,'1',44,4,3);
+insert into puerto values('Puerto F',2500,'E',17,4,2);
+insert into puerto values('Puerto fff',2500,'E',17,4,2);
+insert into puerto values('Puerto nuevo1',2500,'a',17,4,1);
+insert into puerto values('Puerto otro',2500,'E',17,4,1);
+insert into puerto values('Puerto1',2500,'E',23,1,2);
+
+insert into maillot values('MGE','General','Amarillo',1000000);
+insert into maillot values('MMO','MontaÃ±a','Blanco y rojo',500000);
+insert into maillot values('MMS','MÃ¡s sufrido','Estrellitas rojas',400000);
+insert into maillot values('MMV','Metas volantes','Rojo',400000);
+insert into maillot values('MRE','Regularidad','Verde',300000);
+insert into maillot values('MSE','Sprint especial','Rosa',300000);
+
+insert into llevar values(1,3,'MGE');
+insert into llevar values(1,4,'MGE');
+insert into llevar values(2,2,'MGE');
+insert into llevar values(3,1,'MGE');
+insert into llevar values(3,1,'MMV');
+insert into llevar values(3,4,'MRE');
+insert into llevar values(4,1,'MMO');
--- ejercicios-PL_SQL(04)-excepciones.sql
+++ ejercicios-PL_SQL(04)-excepciones.sql
@@ -1,23 +1,59 @@
 --Ejercicios sobre manejos de excepciones:
 
 
-1) Programa parametrizado ('&x') que realiza la operación y/x. Capturar la excepción de dividir por 0 (ZERO_DIVIDE)
+/*1) Programa parametrizado ('&x') que realiza la operación y/x. Capturar la excepción de dividir por 0 (ZERO_DIVIDE)
 
-Este ejercicio es un ejemplo de tratamiento de una sola excepción del sistema con nombre. 
+Este ejercicio es un ejemplo de tratamiento de una sola excepción del sistema con nombre. */
+
+CREATE OR REPLACE PROCEDURE division(num NUMBER)IS
+	res NUMBER;
+BEGIN
+	res:=num/0;
+EXCEPTION
+	WHEN ZERO_DIVIDE THEN 
+		DBMS_OUTPUT.PUT_LINE('Division entre cero');
+END;
+
+
+/*2) Programa parametrizado ('&nombre_ciclista') que cambie el ganador de la primera etapa. Capturar la excepción de que ese 
+ciclista no exista o de que existan varios ciclistas con ese nombre, informando de ello*/
+
+CREATE OR REPLACE PROCEDURE cambia_ganador(nombre_ciclista ciclista.nombre%type)IS
+    v_dorsal ciclista.dorsal%type;
+BEGIN
+    SELECT dorsal INTO v_dorsal FROM ciclista WHERE nombre = nombre_ciclista;
+
+	IF v_dorsal IS NULL THEN
+		RAISE_APPLICATION_ERROR(-2001,'El ciclista ' || nombre_ciclista || ' no existe');
+	ELSE 
+		UPDATE etapa SET dorsal = v_dorsal WHERE netapa = 1;
+    	DBMS_OUTPUT.PUT_LINE('El ganador de la primera etapa ha sido actualizado correctamente.');
+	END IF;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('No se encontró ningún ciclista con ese nombre.');
+    WHEN TOO_MANY_ROWS THEN
+        DBMS_OUTPUT.PUT_LINE('Se encontraron varios ciclistas con ese nombre.');
+END;
 
 
 
-2) Programa parametrizado ('&nombre_ciclista') que cambie el ganador de la primera etapa. Capturar la excepción de que ese ciclista no exista o de que existan varios ciclistas con ese nombre, informando de ello
+
+DECLARE
+    nombre CICLISTA.NOMBRE%TYPE := 'Paco';
+BEGIN
+    cambia_ganador(nombre);
+END;
 
 
-
-3) Redactar un programa parametrizado ('&nombre_puerto', '&altura','&categoria','&pendiente','&numero_etapa', '&nombre_ciclista') que inserte ese puerto. Controlar los posibles casos de error: No exista ese ciclista, que
+/*3) Redactar un programa parametrizado ('&nombre_puerto', '&altura','&categoria','&pendiente','&numero_etapa', '&nombre_ciclista') 
+que inserte ese puerto. Controlar los posibles casos de error: No exista ese ciclista, que
 existan varios ciclistas con ese nombre, no exista esa etapa, el nombre del puerto ya exista, la altura y pendiente
 del puerto sean números.
 
 Tener en cuenta que:
 
-        CICLISTA.dorsal es clave (y por tanto no puede ser NULL)
+    CICLISTA.dorsal es clave (y por tanto no puede ser NULL)
 	ETAPA.netapa es clave (y por tanto UNIQUE y tampoco puede ser NULL)
 	PUERTO.nombre es clave (y por tanto UNIQUE)
 	Ningún otro campo de PUERTO tiene restricción UNIQUE
@@ -28,12 +64,77 @@ Tener en cuenta que:
 Este ejercicio es un ejemplo en el que se ilustra:
 
 -Que se puede lanzar la misma excepción desde diferentes sentencias y son capturadas y manejadas en un solo punto.
--Como se utilizan variables 'dummy' que hacen de 'chivato' para saber que sentencia del programa ha lanzado la excepción, para tratar cada caso de forma diferente si fuera necesario. 
--Las posibles excepciones que se generan dependen de las restricciones establecidas sobre las tablas.
+-Como se utilizan variables 'dummy' que hacen de 'chivato' para saber que sentencia del programa ha lanzado la excepción, 
+para tratar cada caso de forma diferente si fuera necesario. 
+-Las posibles excepciones que se generan dependen de las restricciones establecidas sobre las tablas.*/
 
 
+CREATE OR REPLACE PROCEDURE inserta_puerto(nombre_puerto PUERTO.NOMPUERTO%TYPE, altura PUERTO.altura%TYPE, categoria PUERTO.categoria%TYPE, 
+pendiente PUERTO.categoria%TYPE, numero_etapa ETAPA.NETAPA%TYPE, nombre_ciclista CICLISTA.NOMBRE%TYPE) IS
+    dorsal_ciclista CICLISTA.DORSAL%TYPE;
+    numero_ciclistas NUMBER;
+    etapa_netapa ETAPA.NETAPA%TYPE;
+    puerto VARCHAR2(50);
+    altura_number NUMBER:= TO_NUMBER(altura);
+    pendiente_number NUMBER:= TO_NUMBER(pendiente);
+BEGIN
+	--controla los ciclistas
+	BEGIN
+    SELECT c.dorsal, COUNT(*) INTO dorsal_ciclista, numero_ciclistas
+    FROM ciclista c
+    WHERE c.nombre = nombre_ciclista
+    GROUP BY c.dorsal;
 
-4) Crear un programa PL/SQL parametrizado ('&dorsal','&nombre','&edad','nomeq') que permita insertar un nuevo 
+    IF dorsal_ciclista IS NULL THEN
+      RAISE_APPLICATION_ERROR(-20001, 'El ciclista no fue encontrado.');
+    ELSIF numero_ciclistas > 1 THEN
+      RAISE_APPLICATION_ERROR(-20002, 'Existen varios ciclistas con el mismo nombre.');
+    END IF;
+  	EXCEPTION
+    	WHEN NO_DATA_FOUND THEN
+      		RAISE_APPLICATION_ERROR(-20001, 'El ciclista no fue encontrado.');
+  	END;
+
+	--controla las etapas
+	BEGIN
+		SELECT e.netapa INTO etapa_netapa
+		FROM ETAPA e
+		WHERE e.netapa = numero_etapa;
+
+    IF etapa_netapa IS NULL THEN 
+      RAISE_APPLICATION_ERROR(-20003, 'La etapa no existe.');
+    END IF;
+  	EXCEPTION
+		WHEN NO_DATA_FOUND THEN
+			RAISE_APPLICATION_ERROR(-20003, 'La etapa no existe.');
+  	END;
+
+	--controla los puertos
+	BEGIN
+		SELECT p.nompuerto INTO puerto
+		FROM puerto p
+		WHERE p.nompuerto = nombre_puerto;
+
+    IF puerto IS NOT NULL THEN
+      RAISE_APPLICATION_ERROR(-20004, 'El puerto ya existe.');
+    END IF;
+	EXCEPTION
+		WHEN NO_DATA_FOUND THEN
+		-- El puerto no existe, no se lanza una excepción ya que es un escenario válido
+		NULL;
+	END;
+END;
+
+
+/*prueba*/
+
+DECLARE
+BEGIN
+    inserta_puerto('Picardia',5,'b',20,50,'Mark Sanz');
+END;
+
+
+/*4) Crear un programa PL/SQL parametrizado ('&dorsal','&nombre','&edad','nomeq') que permita insertar un nuevo 
 ciclista garantizando que el dorsal sea un número, el dorsal sea único, que ese equipo exista,
 que la edad sea un número y que sea positivo.
 Nota: guardar el valor del parámetro '&edad' en una variable "edad NUMBER; edad='&edad';" el compilador tratará 
@@ -41,12 +142,22 @@ de hacer una conversión de tipo de cadena a number y si no es posible se produc
 Nota: se asume que el campo 'dorsal' es clave primaria en 'Ciclista' y 'nomeq' es clave ajena en 'Ciclista' respecto a la tabla 'Equipo'
 
 
-Este ejercicio es un ejemplo que ilustra el tratamiento de excepciones en un solo bloque, en el que las excepciones son de los tres tipos posibles (excepciones del sistema con nombre, excepciones del sistema sin nombre y excepciones definidas por el programador).
+Este ejercicio es un ejemplo que ilustra el tratamiento de excepciones en un solo bloque, en el que las excepciones son de los 
+tres tipos posibles (excepciones del sistema con nombre, excepciones del sistema sin nombre y excepciones definidas por el programador).*/
+
+
+CREATE OR REPLACE PROCEDURE insertar_ciclista(dorsal_ciclista CICLISTA.DORSAL%TYPE, nombre_ciclista CICLISTA.NOMBRE%TYPE, edad_ciclista CICLISTA.EDAD%TYPE,
+    nomeq CICLISTA.NOMEQ%TYPE)IS
+dorsal_ciclista NUMBER := TO_NUMBER(dorsal_ciclista);
+BEGIN
+    
+    
+    INSERT INTO ciclista VALUES();
+END;
 
 
 
-
-5) Ampliar el ejercicicio anterior de manera que si la edad propuesta es < 18 años el programa asigna una edad de 18 años al ciclista y lo inserta en la tabla ciclistas.
+/*5) Ampliar el ejercicicio anterior de manera que si la edad propuesta es < 18 años el programa asigna una edad de 18 años al ciclista y lo inserta en la tabla ciclistas.
 Sin embargo, si la edad es superior a 50, el programa considera que es un error y no se realiza la inserción de datos.
 
 Definir un bloque interno en el que se comprueba si la edad es menor que 18, si es el caso se lanza una excepción de usuario (MENOR_EDAD) y se captura en el propio bloque y se trata asignando 18 a la variable edad. Desde el bloque interno se lanzan otras excepciones con (EDAD_NO_POSITIVA) y sin (-20003) nombre, que son capturadas en el bloque externo. El bloque externo, además, captura el resto de excepciones
@@ -65,7 +176,8 @@ Este ejercicio ilustra el tratamiento de excepciones en un bloque anidado, en el
  y resuelta en el bloque interno)
 
 
-Este ejercicio también ilustra como es posible capturar una excepción en un bloque, resolverla y finalizar el bloque sin excepción, lo que permite al bloque mas externo continuar la ejecución normal del programa
+Este ejercicio también ilustra como es posible capturar una excepción en un bloque, resolverla y finalizar el bloque sin excepción, 
+lo que permite al bloque mas externo continuar la ejecución normal del programa*/
 
 
 
